df6d1a0c2bb7d2c2b67e530bd77e13eb597fcf68
Jon <jonathan.stott@gmail.com>
2018-09-16 19:02:29 +0100

    Use enum for measurement types

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,15 +1,18 @@
 BEGIN;
+
+CREATE TYPE measure_type_t AS ENUM('temperature', 'humidity', 'battery');
+
 CREATE TABLE sensor_readings(
     time timestamp with time zone NOT NULL,
     sensor VARCHAR(32) NOT NULL,
-    measure_type VARCHAR(32) NOT NULL,
+    measure_type measure_type_t NOT NULL,
     reading NUMERIC(5, 2) NOT NULL
 );
 CREATE INDEX sensor_time ON sensor_readings USING BRIN (time);
 
 CREATE TABLE sensors(
     sensor VARCHAR(32) NOT NULL,
-    measure_type VARCHAR(32) NOT NULL
+    measure_type measure_type_t NOT NULL
 );
 
 CREATE TABLE sensor_locations(
