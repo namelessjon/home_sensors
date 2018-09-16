89d66ca5acf652df78ebb03ee2698ee7fa073fc5
Jon <jonathan.stott@gmail.com>
2018-09-16 19:06:45 +0100

    Fix indentation

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,24 +1,24 @@
 BEGIN;
 
-CREATE TYPE measure_type_t AS ENUM('temperature', 'humidity', 'battery');
+    CREATE TYPE measure_type_t AS ENUM('temperature', 'humidity', 'battery');
 
-CREATE TABLE sensor_readings(
-    time timestamp with time zone NOT NULL,
-    sensor VARCHAR(32) NOT NULL,
-    measure_type measure_type_t NOT NULL,
-    reading NUMERIC(5, 2) NOT NULL
-);
-CREATE INDEX sensor_time ON sensor_readings USING BRIN (time);
+    CREATE TABLE sensor_readings(
+        time timestamp with time zone NOT NULL,
+        sensor VARCHAR(32) NOT NULL,
+        measure_type measure_type_t NOT NULL,
+        reading NUMERIC(5, 2) NOT NULL
+    );
+    CREATE INDEX sensor_time ON sensor_readings USING BRIN (time);
 
-CREATE TABLE sensors(
-    sensor VARCHAR(32) NOT NULL,
-    measure_type measure_type_t NOT NULL
-);
+    CREATE TABLE sensors(
+        sensor VARCHAR(32) NOT NULL,
+        measure_type measure_type_t NOT NULL
+    );
 
-CREATE TABLE sensor_locations(
-    name VARCHAR(32) NOT NULL,
-    sensor VARCHAR(32) NOT NULL,
-    time_from timestamp with time zone NOT NULL,
-    time_to   timestamp with time zone
-);
+    CREATE TABLE sensor_locations(
+        name VARCHAR(32) NOT NULL,
+        sensor VARCHAR(32) NOT NULL,
+        time_from timestamp with time zone NOT NULL,
+        time_to   timestamp with time zone
+    );
 COMMIT;
